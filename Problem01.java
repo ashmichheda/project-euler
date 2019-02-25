949e47732e87ac98bc2a268cc64fbdeffd092fbd
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-02-25 18:35:23 +0530

    Problem 01

---
+++ Problem01.java
@@ -0,0 +1,16 @@
+package com.prog.ProjectEulerPrograms;
+
+public class Problem01 {
+
+	public static void main(String[] args) {
+
+		// Find the sum of all the multiples of 3 or 5 below 1000
+		int i, sum = 0;
+		for(i = 1; i<1000; i++) {
+			if(i%3 == 0 || i%5 == 0)
+				sum += i;
+		}
+		System.out.println(sum+" is the answer");
+	}
+
+}
