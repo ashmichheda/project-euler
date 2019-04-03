40b5b34f52a75d6efa12fddf46cce673e100a856
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-04-03 22:50:14 +0530

    Add Problem 07

---
+++ Problem07.java
@@ -0,0 +1,35 @@
+// What is the 10 001st prime number?
+
+
+package com.prog.ProjectEulerPrograms;
+
+public class Problem07 {
+
+	public static void main(String[] args) {
+
+		int count = 1;
+		int prime = 3, primeNumber = 0;
+		boolean isPrime = true;
+		while(count != 10001) {
+			isPrime = true;
+			for(int i = 2; i <= prime/2; i++) {
+				
+				if(prime % i == 0) {
+					isPrime = false;
+					break;					
+				}
+			}
+			if(isPrime) {
+				count++;
+				primeNumber = prime;
+				prime++;
+			}
+			else {
+				prime++;
+			}
+		}
+		System.out.println("10001st prime no is: "+primeNumber);
+
+	}
+
+}
