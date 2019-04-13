a65516e3045c69aa581c72fa577fbb3c4d688284
Ashmi Chheda <31774401+ashmichheda@users.noreply.github.com>
2019-04-13 18:22:48 +0530

    add problem 15

---
+++ Problem15.java
@@ -0,0 +1,37 @@
+/*
+ Starting in the top left corner of a 2×2 grid, 
+ and only being able to move to the right and down, 
+ there are exactly 6 routes to the bottom right corner.
+ 
+ How many such routes are there through a 20×20 grid?
+*/
+
+
+package com.prog.ProjectEulerPrograms;
+
+import java.math.BigDecimal;
+
+public class Problem15 {
+
+	public static void main(String[] args) {
+		
+		int numberOfMoves = 40; // 20x20 so 20 + 20 = 40
+		int numberOfRightMoves = 20;
+		
+		System.out.println("No. of routes are: "+factorial(numberOfMoves).divide(factorial(numberOfRightMoves).multiply(factorial(numberOfMoves - numberOfRightMoves))));
+	}
+
+	private static BigDecimal factorial(int n) {
+
+		BigDecimal answer = BigDecimal.ONE;
+		
+		for(int i = 1; i<=n; i++) {
+			answer = answer.multiply(BigDecimal.valueOf(i));
+		}
+		
+		return answer;
+	}
+
+	
+
+}
