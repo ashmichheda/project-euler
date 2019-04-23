5822d3a4f778bb02660ab5d81fa742829ea30872
Ashmi Chheda <ashmichheda@gmail.com>
2019-04-23 23:06:21 +0530

    add Problem 25

---
+++ Problem25.java
@@ -0,0 +1,30 @@
+package com.prog.ProjectEulerPrograms;
+
+import java.math.BigInteger;
+
+public class Problem25 {
+
+	public static void main(String[] args) {
+		
+		int index = 2;
+		BigInteger f1 = BigInteger.ONE;
+		BigInteger f2 = new BigInteger("1");
+		BigInteger fib = new BigInteger("0");
+		long firstThousandthTerm;
+		while(true) {
+			
+			fib = f1.add(f2);
+			index++;
+			if((""+fib).length() == 1000) {
+				firstThousandthTerm = index;
+				break;
+			}
+			else {
+				f1 = f2;
+				f2 = fib;
+			}
+		}
+		System.out.println("Index of the 1st 1000th fib term is: "+firstThousandthTerm);
+	}
+
+}
