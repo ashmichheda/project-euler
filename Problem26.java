e63dfc360acabe7f835fb292076786b791d207e1
Ashmi Chheda <ashmichheda@gmail.com>
2019-04-25 23:38:48 +0530

    add problem 26

---
+++ Problem26.java
@@ -0,0 +1,37 @@
+package com.prog.ProjectEulerPrograms;
+
+public class Problem26 {
+
+	public static void main(String[] args) {
+		
+		int sequenceLength = 0;
+        int num = 0;
+        long start = System.currentTimeMillis();
+        for (int i = 1000; i > 1; i--) {
+            if (sequenceLength >= i) {
+                break;
+            }
+
+            int[] foundRemainders = new int[i];
+            int value = 1;
+            int position = 0;
+
+            while (foundRemainders[value] == 0 && value != 0) {
+                foundRemainders[value] = position;
+                value *= 10;
+                value %= i;
+                position++;
+            }
+
+            if (position - foundRemainders[value] > sequenceLength) {
+                num = i;
+                sequenceLength = position - foundRemainders[value];                    
+            }
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Value of d is: "+num);
+        System.out.println("Execution time in milliseconds: "+(end - start)+" ms");
+        
+	}
+
+}
