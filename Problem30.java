384b735bf358034d8a7afc6edce9b381878af561
Ashmi Chheda <ashmichheda@gmail.com>
2019-04-30 23:26:35 +0530

    added upper bound logic

--- Problem30.java
+++ Problem30.java
@@ -1,5 +1,9 @@
 //Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 
+/* Straight forward problem, but trick here is to find an upper bound.
+ Say 9^5 + 9^5 + 9^5 + 9^5 + 9^5 = 295245, say multiplying it 6 times 6*9^5 = 354294, 
+ to round up 355000 can be considered an upper bound */
+ 
 package com.prog.ProjectEulerPrograms;
 
 public class Problem30 {
@@ -10,7 +14,7 @@ public class Problem30 {
 		int i, j, sum = 0, temp, answer = 0;
 		// start time
 		long start = System.currentTimeMillis();
-		for(i = 2; i<999999; i++) {
+		for(i = 2; i<355000; i++) {
 			
 			temp = i;
 			
